a26e46be7fea9553d5143ecc23febc10321cba71
Charlles <matheuscharles453@gmail.com>
2022-05-02 01:39:27 -0300

    refactor

--- src/App.java
+++ src/App.java
@@ -21,45 +21,37 @@ public class App {
 
         //*********************************CALCULO DO PRIMEIRO DIGITO*****************************
 
-          for (int posicaoDigito = 0; posicaoDigito < 9; posicaoDigito++) {
-            recebeDigito= cpf.charAt(posicaoDigito);
-                     
-            NumeroCpf = Integer.parseInt(String.valueOf(recebeDigito));
-
-          
-            recebeSomaCpf += NumeroCpf * baseMultiplicadora; 
-            baseMultiplicadora--;
-
-          }  
-        
+          recebeSomaCpf = CalculaPenultinoDigito(cpf, baseMultiplicadora, recebeSomaCpf);          
         
 
         PenultimoDigito = totalDigitosCpf - (recebeSomaCpf % totalDigitosCpf);
         
 
-        recebeDigito= cpf.charAt(9);                     
-        NumeroCpf = Integer.parseInt(String.valueOf(recebeDigito));
+        NumeroCpf = ComparaPenultimoDigito(cpf);
 
-        // valida o penultimo digito do cpf
-        if(PenultimoDigito == NumeroCpf){
-
-            PenultimoDigitoValido = true;           
-        }else if(PenultimoDigito >= 10){
-
-            PenultimoDigito = 0;
-
-            if(PenultimoDigito == NumeroCpf){
-
-                PenultimoDigitoValido = true;  
-            }else{
-
-                PenultimoDigitoValido = false;
-            }
-        }
+        PenultimoDigitoValido = ValidaUltimoDigito(PenultimoDigitoValido, PenultimoDigito, NumeroCpf);
 
      
 
         //***********************************CALCULO ULTIMO DIGITO DO CPF********************************
+        UltimoDigitoValido = CalculaUltimoDigito(cpf, recebeSomaCpf, PenultimoDigitoValido, UltimoDigitoValido);
+
+        // valida o ultimo digito do cpf       
+
+        validaCPF(PenultimoDigitoValido, UltimoDigitoValido);      
+
+
+
+        input.close();
+
+    }
+
+    private static boolean CalculaUltimoDigito(String cpf, int recebeSomaCpf, boolean PenultimoDigitoValido,
+        boolean UltimoDigitoValido) {
+        int baseMultiplicadora;
+        int UltimoDigito;
+        int NumeroCpf;
+        char recebeDigito;
         if(PenultimoDigitoValido){
 
             //resetar valores importantes            
@@ -88,10 +80,15 @@ public class App {
 
         UltimoDigito = 11 - (recebeSomaCpf % 11);        
 
-        recebeDigito= cpf.charAt(10);                     
-        NumeroCpf = Integer.parseInt(String.valueOf(recebeDigito));
+        NumeroCpf = ComparaUltimoDigitoDigito(cpf);
 
         //VAlida o ultimo digito do cpf
+        UltimoDigitoValido = ValidaUltimoDigito(UltimoDigitoValido, UltimoDigito, NumeroCpf);
+
+        return UltimoDigitoValido;
+    }
+
+    private static boolean ValidaUltimoDigito(boolean UltimoDigitoValido, int UltimoDigito, int NumeroCpf) {
         if(UltimoDigito == NumeroCpf){
 
             UltimoDigitoValido = true;           
@@ -107,18 +104,47 @@ public class App {
                 UltimoDigitoValido = false;
             }
         }
+        return UltimoDigitoValido;
+    }
 
-        // valida o ultimo digito do cpf       
+    private static int ComparaPenultimoDigito(String cpf) {
+        int NumeroCpf;
+        char recebeDigito;
+        recebeDigito= cpf.charAt(9);                     
+        NumeroCpf = Integer.parseInt(String.valueOf(recebeDigito));
+        return NumeroCpf;
+    }
 
+    private static int CalculaPenultinoDigito(String cpf, int baseMultiplicadora, int recebeSomaCpf) {
+        int NumeroCpf;
+        char recebeDigito;
+        for (int posicaoDigito = 0; posicaoDigito < 9; posicaoDigito++) {
+              
+            recebeDigito= cpf.charAt(posicaoDigito);
+                     
+            NumeroCpf = Integer.parseInt(String.valueOf(recebeDigito));
+
+          
+            recebeSomaCpf += NumeroCpf * baseMultiplicadora; 
+            baseMultiplicadora--;
+
+          }
+        return recebeSomaCpf;
+    }
+
+    private static int ComparaUltimoDigitoDigito(String cpf) {
+        int NumeroCpf;
+        char recebeDigito;
+        recebeDigito= cpf.charAt(10);                     
+        NumeroCpf = Integer.parseInt(String.valueOf(recebeDigito));
+        return NumeroCpf;
+    }
+
+    private static void validaCPF(boolean PenultimoDigitoValido, boolean UltimoDigitoValido) {
         if(UltimoDigitoValido && PenultimoDigitoValido){
                 System.out.println("CPF VALIDADO COM SUCESSO!");
         }else{
             System.out.println("CPF INVALIDO!");
-        }      
-
-
-
-        input.close();
-
+        }
     }
 }
